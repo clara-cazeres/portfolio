f3225fa4d802630a10183df8bb140fa03676ed6c
clara-cazeres <claracazeres@gmail.com>
2024-05-10 21:46:24 -0300

    actualizaciones

--- scripts/script.js
+++ scripts/script.js
@@ -30,56 +30,23 @@ function listarProyectos() {
 }
 
 
-/* document.addEventListener('DOMContentLoaded', function() {
-    listarProyectos();
-});
-
-function handleIntersect(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // Elemento entra en la vista - Anima entrando
-            entry.target.style.opacity = 1;
-            entry.target.style.transform = 'translateY(0)';
-        } else {
-            // Elemento sale de la vista - Anima saliendo
-            entry.target.style.opacity = 0;
-            entry.target.style.transform = 'translateY(20px)';
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    const flecha = document.querySelector('#icon-flecha .material-symbols-outlined');
+    flecha.addEventListener('click', function() {
+      // Detiene la animación al hacer clic
+      flecha.style.animation = 'none';
+      // Calcula la posición y realiza el scroll
+      const proyectosSection = document.querySelector('#s-proyectos');
+      if (proyectosSection) {
+        const topPos = proyectosSection.getBoundingClientRect().top + window.scrollY - 50; // Usando scrollY en lugar de pageYOffset
+        window.scrollTo({
+          top: topPos,
+          behavior: 'smooth'
+        });
+      }
     });
-}
+  });
+  
+  
+  
 
-function listarProyectos() {
-    fetch('http://localhost:3000/proyectos')
-        .then(response => response.json())
-        .then(proyectos => {
-            const divProyectos = document.getElementById('container-proyectos');
-            proyectos.forEach(proyecto => {
-                const article = document.createElement('article');
-                article.className = 'project-article';
-                article.innerHTML = `
-                    <img src="img/${proyecto.miniatura}" alt="${proyecto.titulo}" class="project-image">
-                    <div class="hover-info">
-                        <h3>${proyecto.titulo}</h3>
-                        <p>${proyecto.categoria}</p>
-                    </div>
-                `;
-                divProyectos.appendChild(article);
-            });
-
-            
-
-            // Configura el observer
-            const observer = new IntersectionObserver(handleIntersect, {
-                root: null,  // Default: viewport
-                rootMargin: '0px',
-                threshold: 0.1  // Ajusta según lo que necesites para la sensibilidad
-            });
-
-            // Observa cada artículo
-            document.querySelectorAll('.project-article').forEach(article => {
-                observer.observe(article);
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
- */
